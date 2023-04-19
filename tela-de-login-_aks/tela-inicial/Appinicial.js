50cb805828115439d70dd097d4c4fece7124a07b
Pedrix55 <pedrogabriel12gamer@outlook.com>
2023-04-19 14:50:07 -0300

    ASK - POST 1

---
+++ tela-de-login-_aks/tela-inicial/Appinicial.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react'
+import {View, Button} from 'react-native';
+
+const Inicial = () => {
+  return (
+  <View >
+  <Button title='Jogar'/>
+  </View>
+)};
+
+export default Inicial;
